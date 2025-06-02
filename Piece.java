e27b89c82aba1fa63fcadd59f172f8d044b53bd8
auro <sarrm20@icloud.com>
2025-06-02 16:24:11 +0000

    Piece

--- Piece.java
+++ Piece.java
@@ -1,21 +1,43 @@
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Piece {
     public BufferedImage image;
-    public int col, row;
-    public int xPos, yPos;
+    public int col, row, preCol, preRow;
+    public int color;
+    public int x, y;
 
-    public Piece(BufferedImage image, int col, int row) {
-        this.image = image;
+    public Piece(int color, int col, int row) {
+        this.color = color;
         this.col = col;
         this.row = row;
-        this.xPos = col * Board.SQUARE_SIZE;
-        this.yPos = row * Board.SQUARE_SIZE;
+        x = getX(col);
+        y = getY(row);
+        preCol = col;
+        preRow = row;
 
     }
 
-    public void draw(Graphics2D g2) {
-        g2.drawImage(image, xPos, yPos, Board.SQUARE_SIZE, Board.SQUARE_SIZE, null);
+    public BufferedImage getImage(String imagePath) {
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+        
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
+
+    public int getX(int col) {
+        return col * Board.SQUARE_SIZE;
+    }
+
+    public int getY(int row) { 
+        return row * Board.SQUARE_SIZE;
+    }
+
 }
